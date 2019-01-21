d5e62643ad90fd8b903a93fcf071eb5cd440d2a9
jtourvieille <jtourvieille@gmail.com>
2019-01-21 13:49:23 +0100

    Add singleton exercice

---
+++ src/CS.Impl/04_Advanced/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System.Dynamic;
+
+namespace CS.Impl._04_Advanced
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private Singleton()
+        {
+            
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+
+                return _instance;
+            }
+        }
+    }
+
+    public interface IMySingleton { }
+    public class MySingleton : IMySingleton { }
+}
---
+++ src/CS.Tests/04_Advanced/SingletonTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using CS.Impl._04_Advanced;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CS.Tests._04_Advanced
+{
+    [TestClass]
+    public class SingletonTests
+    {
+        [TestMethod]
+        public void GetTheSameInstanceOfSimpleSingleton()
+        {
+            // Arrange & act
+            var myObject1 = new object();
+            var myObject2 = new object();
+            var singleton1 = Singleton.Instance;
+            var singleton2 = Singleton.Instance;
+
+            // Assert
+            Assert.AreNotEqual(myObject1.GetHashCode(), myObject2.GetHashCode());
+            Assert.AreEqual(singleton2.GetHashCode(), singleton1.GetHashCode());
+        }
+
+        [TestMethod]
+        public void GetTheSameInstanceOfSingletonUsingIoC()
+        {
+            // Arrange
+            using (var serviceProvider = new ServiceCollection().AddSingleton<IMySingleton, MySingleton>().BuildServiceProvider())
+            {
+                // Act
+                var singleton1 = serviceProvider.GetService<IMySingleton>();
+                var singleton2 = serviceProvider.GetService<IMySingleton>();
+
+                // Assert
+                Assert.AreEqual(singleton2.GetHashCode(), singleton1.GetHashCode());
+            }
+        }
+    }
+}
