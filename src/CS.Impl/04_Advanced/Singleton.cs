39c885f83a582f50aae6557d0d1ea927908a7a5f
jtourvieille <jtourvieille@gmail.com>
2019-01-21 14:36:20 +0100

    Add Singleton exercice

---
+++ src/CS.Impl/04_Advanced/Singleton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS.Impl._04_Advanced
+{
+    public class Singleton
+    {
+        public static Singleton Instance => throw new NotImplementedException();
+    }
+
+    public interface IMySingleton { }
+    public class MySingleton : IMySingleton { }
+}
