8f17c8bcd846aab2bebbf07965a3cb9e5cbcddea
Charles Gilles <charles.gilles@axa.fr>
2022-11-18 14:21:24 +0100

    feat(Lambdas): Ajout des réponses aux exercices sur les lambdas

---
+++ src/CS.Tests/05_Lambdas/LambdasWithLinqTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using static CS.Impl._05_Lambdas.LambdasWithLinq;
+
+namespace CS.Tests._05_Lambdas
+{
+    [TestClass]
+    public class LambdasWithLinqTests
+    {
+        [TestMethod]
+        public void TestGetMoyenneDesPrixDesChaisesA4PeidsParMateriaux()
+        {
+            // Arrange
+            var chaises = new List<Chaise>()
+            {
+                new Chaise { Materiaux = Materiaux.Bois, Pieds = 4, Prix = 100},
+                new Chaise { Materiaux = Materiaux.Bois, Pieds = 4, Prix = 200},
+                new Chaise { Materiaux = Materiaux.Plastique, Pieds = 4, Prix = 300},
+                new Chaise { Materiaux = Materiaux.Plastique, Pieds = 3, Prix = 400},
+                new Chaise { Materiaux = Materiaux.Plastique, Pieds = 3, Prix = 500},
+                new Chaise { Materiaux = Materiaux.Metal, Pieds = 4, Prix = 600},
+            };
+
+            var expected = new Dictionary<Materiaux, int>()
+            {
+                {Materiaux.Bois, 150 },
+                {Materiaux.Plastique, 300 },
+                {Materiaux.Metal, 600 },
+            };
+            // Act
+
+            var result = GetMoyenneDesPrixDesChaisesA4PeidsParMateriaux(chaises);
+
+            // Assert
+            foreach (var materiaux in Enum.GetValues(typeof(Materiaux)) as IEnumerable<Materiaux>)
+            {
+                Assert.AreEqual(expected[materiaux], result[materiaux]);
+            }
+        }
+    }
+}
