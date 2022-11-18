65c70fd6b1e78d95caade00d3eddbe2017be2223
Charles Gilles <charles.gilles@axa.fr>
2022-11-18 11:41:45 +0100

    feature(Lambdas): Ajout d'exercices sur les expressions lambdas

---
+++ src/CS.Impl/05_Lambdas/BasicLambdas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CS.Impl._05_Lambdas
+{
+    public static class BasicLambdas
+    {
+        // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/lambda-expressions
+
+        public static Func<int, int, int> GetAdditionFunc()
+        {
+            return null;
+        }
+
+        public static Func<string> GetHelloWorldFunc()
+        {
+            return null;
+        }
+    }
+}
